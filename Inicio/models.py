f6dc418db94343e3f1e6deeaf4e74ed2c7dfa416
Javier <javier.navarro.a@usach.cl>
2019-06-17 20:15:24 -0400

    Añadida la fecha de lanzamiento
    Default 00/00/0000

--- Inicio/models.py
+++ Inicio/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from datetime import datetime
 
 class Videojuego(models.Model):
     caratula = models.CharField(max_length = 500)
     nombre = models.CharField(max_length = 50)
+    fecha = models.CharField(max_length = 50)
 
     def __str__(self):
-        return self.caratula + ' I ' + self.nombre
+        return self.caratula + ' I ' + self.nombre + ' I ' + self.fecha
