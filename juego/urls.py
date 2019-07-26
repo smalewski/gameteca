8ff6bb46c1f111908d4f6205df292eee1ee718e5
Stefan Malewski <lem@lemnote.chongos>
2019-07-26 12:24:10 -0400

    Fresh start

--- juego/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('<int:videojuego_id>/',views.index, name = 'index'),
-    path('<str:videojuego_nombre>/',views.busqueda, name = 'index'),
-]
