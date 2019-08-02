79642d61776dab91a6c7d0e4d2782cc2073325e2
Stefan Malewski <lem@lemnote.chongos>
2019-08-02 13:52:40 -0400

    Procfile

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Logs
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
