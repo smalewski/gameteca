e13dff10a14e15cbf54ece89ff36dfbbf8bf0cc3
Stefan Malewski <lem@lemnote.chongos>
2019-08-07 12:51:16 -0400

    Roles con admin

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
+from functools import wraps
 from os import environ
 
 app = Flask(__name__)
@@ -15,8 +16,6 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes, models
-
 # Sesiones
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -25,3 +24,20 @@ login_manager.login_view = 'crearCuenta'
 @login_manager.user_loader
 def load_user(user_id):
     return models.Usuario.query.get(user_id)
+
+from app import models
+
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            if not current_user.is_authenticated():
+              return login_manager.unauthorized()
+            if ((current_user.urole() != role) and (role != "ANY")):
+                return login_manager.unauthorized()
+            return fn(*args, **kwargs)
+        return decorated_view
+    return wrapper
+
+
+from app import routes
